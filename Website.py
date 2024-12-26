3d3970fb1d1cabae6dde75b4b573b5a6d4a5270f
Sathvik Karanth <karanthsathvik@gmail.com>
2024-12-26 13:46:55 +0530

    modified

--- Website.py
+++ Website.py
@@ -1,45 +1,26 @@
-from flask import Flask,render_template,request,redirect
-from flask_cors import CORS,cross_origin
-import pickle
+import streamlit as st
 import pandas as pd
+import pickle
 import numpy as np
 import sklearn
-import streamlit
 
-app=Flask(__name__)
-cors=CORS(app)
 model=pickle.load(open('LinearRegressionModel.pkl','rb'))
+
 car=pd.read_csv('Cleaned_cars.csv')
+companies=sorted(car['company'].unique())
+car_models=sorted(car['name'].unique())
+year=sorted(car['year'].unique(),reverse=True)
+fuel_type=car['fuel_type'].unique()
 
-@app.route('/',methods=['GET','POST'])
-def index():
-    companies=sorted(car['company'].unique())
-    car_models=sorted(car['name'].unique())
-    year=sorted(car['year'].unique(),reverse=True)
-    fuel_type=car['fuel_type'].unique()
+st.title("Car Price Predictor")
 
-    companies.insert(0,'Select Company')
-    return render_template('index.html',companies=companies, car_models=car_models, years=year,fuel_types=fuel_type)
+car_company = st.selectbox("Which company does your car belong to?", companies)
+car_model = st.selectbox("Which car model is it?", car[car['company']==car_company]['name'].unique())
+car_year = st.selectbox("Which year was the car bought?", year)
+car_fuel_type = st.selectbox("Fuel type?", fuel_type)
+kms_driven = st.number_input('kms_driven')
 
-
-@app.route('/predict',methods=['POST'])
-@cross_origin()
-def predict():
-
-    company=request.form.get('company')
-
-    car_model=request.form.get('car_models')
-    year=request.form.get('year')
-    fuel_type=request.form.get('fuel_type')
-    driven=request.form.get('kilo_driven')
-
-    prediction=model.predict(pd.DataFrame(columns=['name', 'company', 'year', 'kms_driven', 'fuel_type'],
-                              data=np.array([car_model,company,year,driven,fuel_type]).reshape(1, 5)))
-    print(prediction)
-
-    return str(np.round(prediction[0],2))
-
-
-
-if __name__=='__main__':
-    app.run(port=5000)
+if st.button('Predict'):
+    prediction=model.predict(pd.DataFrame(columns=['name', 'company', 'year', 'kms_driven', 'fuel_type'], 
+                                          data=np.array([car_model,car_company,car_year,kms_driven,car_fuel_type]).reshape(1, 5)))
+    st.text("Rs. "+str(int(prediction[0])))
